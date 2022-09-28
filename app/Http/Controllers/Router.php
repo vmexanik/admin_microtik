8921fc768e3b0d0d61f710a051250d318cdc5ad5
vmexanik <vmexanik.work@gmail.com>
2022-09-28 17:41:36 +0300

    круд на роутеры

---
+++ app/Http/Controllers/Router.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Fop;
+use Exception;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Validator;
+use RouterOS\Client;
+use RouterOS\Config;
+use RouterOS\Exceptions\BadCredentialsException;
+use RouterOS\Exceptions\ClientException;
+use RouterOS\Exceptions\ConfigException;
+use RouterOS\Exceptions\ConnectException;
+use RouterOS\Exceptions\QueryException;
+
+class Router extends Controller
+{
+    /**
+     * @throws ClientException
+     * @throws ConnectException
+     * @throws BadCredentialsException
+     * @throws QueryException
+     * @throws ConfigException
+     */
+    public function connect()
+    {
+//
+//        83.150.204.82:8728
+//vmexanik : jMap9Kp2QnsH3h
+
+        $errorMsg='';
+
+        try{
+            $config = new Config([
+                'host' => '83.150.204.82',
+                'user' => 'vmexanik',
+                'pass' => 'jMap9Kp2QnsH3h',
+                'port' => 8728,
+            ]);
+            $client = new Client($config);
+        }catch (Exception $e){
+            $errorMsg = mb_convert_encoding($e->getMessage(),'UTF-8');
+        }
+
+
+        return view('welcome', [
+            'error'=>$errorMsg
+        ]);
+    }
+
+    public function index(): Factory|View|Application
+    {
+        $routers=\App\Models\Router::orderBy('id', 'asc')->get();
+
+        return view('router/list',[
+            'routers'=>$routers
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param Request $request
+     * @return Application|RedirectResponse|Redirector
+     */
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(),
+            [
+                'name' => 'required',
+                'ip_address'=>'required|ip',
+                'login'=>'required',
+                'password'=>'required',
+                'port'=>'required'
+            ]);
+
+        if ($validator->fails()) {
+            return redirect('/router')
+                ->withInput()
+                ->withErrors($validator);
+        }
+
+        $router = new \App\Models\Router();
+        $router->name = $request->name;
+        $router->ip_address = $request->ip_address;
+        $router->login = $request->login;
+        $router->password = $request->password;
+        $router->port = $request->port;
+        $router->save();
+
+        return redirect('/router')->with('status','Сохранение успешно!');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     * @return void
+     */
+    public function store(Request $request): void
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param \App\Models\Router $fop
+     * @return void
+     */
+    public function show(\App\Models\Router $fop): void
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param \App\Models\Router $fop
+     * @return Response
+     */
+    public function edit(\App\Models\Router $fop)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param \App\Models\Router $fop
+     * @return Response
+     */
+    public function update(Request $request, \App\Models\Router $fop)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Router $router
+     * @return Redirector|Application|RedirectResponse
+     */
+    public function destroy(\App\Models\Router $router): Redirector|Application|RedirectResponse
+    {
+        $router->delete();
+
+        return redirect('/router')->with('status','Удаление успешно!');
+    }
+}
---
+++ database/migrations/2022_09_28_134759_create_router_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('routers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->ipAddress();
+            $table->string('login');
+            $table->string('password');
+            $table->integer('port');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('routers');
+    }
+};
