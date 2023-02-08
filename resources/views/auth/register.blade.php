0950165c44cb36f874e532697de8e9f799a12d34
Artem Volokhovych <doker@starlink.ua>
2023-02-08 15:48:11 +0000

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
 @include('header')
 <main class="flex-shrink-0">
     <div class="container">
-        <h1 class="mt-4 mb-5">Регистрация пользователей</h1>
+        <h1 class="mt-4 mb-5">Register new admin</h1>
         <div class="row justify-content-md-center">
             @if (session('errors'))
                 <div class="alert alert-danger">
@@ -20,30 +20,30 @@
                       action="{{ route('register') }}">
                     @csrf
                     <div class="row m-0 mb-2">
-                        <label for="name" class="form-label">Имя</label>
+                        <label for="name" class="form-label">Username</label>
                         <input name="name" type="text" class="form-control" id="name" placeholder=""
                                required autofocus value="{{old('name')}}">
                     </div>
                     <div class="row m-0 mb-2">
-                        <label for="password" class="form-label">Пароль</label>
+                        <label for="password" class="form-label">Password</label>
                         <input name="password" type="text" class="form-control" id="password" placeholder=""
                                required autocomplete="new-password">
                     </div>
                     <div class="row m-0 mb-2">
-                        <label for="password_confirmation" class="form-label">Подтверждение пароля</label>
+                        <label for="password_confirmation" class="form-label">Confirm Password</label>
                         <input name="password_confirmation" type="text" class="form-control" id="password_confirmation" placeholder=""
                                required >
                     </div>
                     <div class="row m-0 mb-2">
-                        <button type="submit" class="btn btn-primary">Зарегистрировать</button>
+                        <button type="submit" class="btn btn-primary">Register</button>
                     </div>
                 </form>
                 <table class="table table-hover mt-3 align-middle">
                     <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Имя</th>
-                        <th scope="col">Действие</th>
+                        <th scope="col">Username</th>
+                        <th scope="col">Action</th>
                     </tr>
                     </thead>
                     @foreach ($users as $user)
@@ -66,7 +66,7 @@
                                             @method('DELETE')
                                             <input type="hidden" name="id" value="{{$user->id}}">
                                             <button type="submit" class="btn btn-danger">
-                                                <i class="fa fa-trash"></i> Удалить
+                                                <i class="fa fa-trash"></i> Delete
                                             </button>
                                         </form>
                                     @endif
@@ -74,7 +74,7 @@
                                         @csrf
                                         <input type="hidden" name="id" value="{{$user->id}}">
                                         <button type="submit" class="btn btn-danger">
-                                            <i class="fa fa-trash"></i> Изменить пароль
+                                            <i class="fa fa-trash"></i> Change password
                                         </button>
                                     </form>
                                 </div>
